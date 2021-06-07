12685280dcbeae137e4f7b98e5808b22433f2053
Anwesh43 <anweshthecool0@gmail.com>
2021-06-08 01:02:24 +0530

    adding EitherSideBallShooter class

--- eithersideballshooterview/src/main/java/com/example/eithersideballshooterview/EitherSideBallShooterView.kt
+++ eithersideballshooterview/src/main/java/com/example/eithersideballshooterview/EitherSideBallShooterView.kt
@@ -161,4 +161,27 @@ class EitherSideBallShooterView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class EitehrSideBallShooter(var i : Int, val state : State = State()) {
+
+        private var curr : ESBSNode = ESBSNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }

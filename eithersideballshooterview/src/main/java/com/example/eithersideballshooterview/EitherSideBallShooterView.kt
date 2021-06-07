36a91cd12b38d230ecf211d832c9a42653d31339
Anwesh43 <anweshthecool0@gmail.com>
2021-06-08 00:52:43 +0530

    adding Canvas extension methods

--- eithersideballshooterview/src/main/java/com/example/eithersideballshooterview/EitherSideBallShooterView.kt
+++ eithersideballshooterview/src/main/java/com/example/eithersideballshooterview/EitherSideBallShooterView.kt
@@ -24,7 +24,38 @@ val sizeFactor : Float = 4.9f
 val rFactor : Float = 11.2f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawEitherSideBallShooter(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    val r : Float = Math.min(w, h) / rFactor
+    save()
+    translate(w / 2, h / 2)
+    rotate(deg * sc3)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        drawLine(0f, 0f, size * (sc2 - sc5), 0f, paint)
+        drawCircle(size * sc2 + (h / 2 - size + r) * sc4, 0f, r * sc1, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawESBSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawEitherSideBallShooter(scale, w, h, paint)
+}

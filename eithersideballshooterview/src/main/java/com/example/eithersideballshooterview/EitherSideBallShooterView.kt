df404ddeec62b27904b328a1a1443b0a4d28bb37
Anwesh43 <anweshthecool0@gmail.com>
2021-06-08 00:39:11 +0530

    adding constants and libraries

---
+++ eithersideballshooterview/src/main/java/com/example/eithersideballshooterview/EitherSideBallShooterView.kt
@@ -0,0 +1,26 @@
+package com.example.eithersideballshooterview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#673AB7",
+    "#00C853",
+    "#304FFE",
+    "#BF360C"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 5
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val rFactor : Float = 11.2f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
